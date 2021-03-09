468a48c874441fc526c81f163bfd6e4876959c71
vandan76 <80036659+vandan76@users.noreply.github.com>
2021-03-09 11:54:55 +0530

    Update example.py

--- example.py
+++ example.py
@@ -10,26 +10,26 @@ try:
         sys.stdout.write("\r")
         sys.stdout.write("\033[K")
         sys.stdout.write("CO: %g ppm" % (perc["CO"]))
-        if perc["co"] <=4.4:
-            ans=((50-0)/(4.4-0))*(perc["co"]-0)+0
+        if perc["CO"] <=4.4:
+            ans=((50-0)/(4.4-0))*(perc["CO"]-0)+0
             sys.stdout.write("AQI:",ans)
-        elif 4.4>perc["co"] and 9.5>perc["co"]:
-            ans=((100-51)/(9.4-4.5))*(perc["co"]-4.5)+51
+        elif 4.4>perc["CO"] and 9.5>perc["CO"]:
+            ans=((100-51)/(9.4-4.5))*(perc["CO"]-4.5)+51
             sys.stdout.write("AQI:",ans)
-        elif 9.4>perc["co"] and 12.5>perc["co"]:
-            ans=((150-101)/(12.4-9.5))*(perc["co"]-9.5)+101
+        elif 9.4>perc["CO"] and 12.5>perc["CO"]:
+            ans=((150-101)/(12.4-9.5))*(perc["CO"]-9.5)+101
             sys.stdout.write("AQI:",ans)
-        elif 12.4>perc["co"] and 15.5>perc["co"]:
-            ans=((200-151)/(15.4-12.5))*(perc["co"]-12.5)+151
+        elif 12.4>perc["CO"] and 15.5>perc["CO"]:
+            ans=((200-151)/(15.4-12.5))*(perc["CO"]-12.5)+151
             sys.stdout.write("AQI:",ans)
-        elif 15.4>perc["co"] and 30.5>perc["co"]:
-            ans=((300-201)/(30.4-15.5))*(perc["co"]-15.5)+201
+        elif 15.4>perc["CO"] and 30.5>perc["CO"]:
+            ans=((300-201)/(30.4-15.5))*(perc["CO"]-15.5)+201
             sys.stdout.write("AQI:",ans)
-        elif 30.4>perc["co"] and 40.5>perc["co"]:
-            ans=((400-301)/(40.5-30.5))*(perc["co"]-30.5)+301
+        elif 30.4>perc["CO"] and 40.5>perc["CO"]:
+            ans=((400-301)/(40.5-30.5))*(perc["CO"]-30.5)+301
             sys.stdout.write("AQI:",ans)
-        elif 40.4>perc["co"] and 50.5>perc["co"]:
-            ans=((500-401)*(50.5-40.5))(perc["co"]-40.5)+401
+        elif 40.4>perc["CO"] and 50.5>perc["CO"]:
+            ans=((500-401)*(50.5-40.5))(perc["CO"]-40.5)+401
             sys.stdout.write("AQI:",ans)
         sys.stdout.flush()
         time.sleep(0.1)
