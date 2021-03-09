9a1d5ff79634287609ab0a791dff9c1b9fabcfdd
Arun M Solanki <akashsolanki292@gmail.com>
2021-03-09 12:07:34 +0530

    Update example.py

--- example.py
+++ example.py
@@ -11,28 +11,26 @@ try:
         sys.stdout.write("\033[K")
         sys.stdout.write("CO: %g ppm" % (perc["CO"]))
         if perc["CO"] <=4.4:
-            sys.stdout.write("vandan")
             ans=((50-0)/(4.4-0))*(perc["CO"]-0)+0
-            sys.stdout.write("joshi")
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 4.4>perc["CO"] and 9.5>perc["CO"]:
             ans=((100-51)/(9.4-4.5))*(perc["CO"]-4.5)+51
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 9.4>perc["CO"] and 12.5>perc["CO"]:
             ans=((150-101)/(12.4-9.5))*(perc["CO"]-9.5)+101
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 12.4>perc["CO"] and 15.5>perc["CO"]:
             ans=((200-151)/(15.4-12.5))*(perc["CO"]-12.5)+151
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 15.4>perc["CO"] and 30.5>perc["CO"]:
             ans=((300-201)/(30.4-15.5))*(perc["CO"]-15.5)+201
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 30.4>perc["CO"] and 40.5>perc["CO"]:
             ans=((400-301)/(40.5-30.5))*(perc["CO"]-30.5)+301
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         elif 40.4>perc["CO"] and 50.5>perc["CO"]:
             ans=((500-401)*(50.5-40.5))(perc["CO"]-40.5)+401
-            sys.stdout.write("AQI:",ans)
+            sys.stdout.write("AQI: %g" % ans)
         sys.stdout.flush()
         time.sleep(0.1)
 except:
