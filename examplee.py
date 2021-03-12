4f3147ece08b681c0f7eb07dfea923aa1fd50567
vandan76 <80036659+vandan76@users.noreply.github.com>
2021-03-12 12:30:00 +0530

    Update examplee.py

--- examplee.py
+++ examplee.py
@@ -1,18 +1,20 @@
-
-import example
-import Adafruit_CharLCD as LCD
+import Adafruit_CharLCD  as LCD
 import RPi.GPIO as GPIO
+import example
 import time
 
 GPIO.setmode(GPIO.BCM)
-lcd1 = 12
+
+lcd1 = 12 
 lcd2 = 7
 lcd3 = 21
 lcd4 = 25
 lcd5 = 24
 lcd6 = 23
 
-lcd = LCD.Adafruit_charLCD(lcd1 ,lcd2 ,lcd3 ,lcd4 ,lcd5 ,lcd6, 0, 16, 2)
+lcd = LCD.Adafruit_CharLCD(lcd1, lcd2, lcd3, lcd4, lcd5, lcd6, 0, 16, 2)
+
+
 
 while True :
     x=test.ans
